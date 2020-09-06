f6829ebe3ebabf8f6f816ea860afd0027d6acb60
George Shuklin <george.shuklin@gmail.com>
2020-09-06 10:29:46 +0300

    Switch to old code with mem fix.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,37 @@
 extern crate piston_window;
 extern crate image as im;
-extern crate vecmath;
 
 use piston_window::*;
-use vecmath::*;
+use piston::event_loop::Events;
 
 fn main() {
+    let x = 1920;
+    let y  = 1080;
     let opengl = OpenGL::V3_2;
-    let (width, height) = (300, 300);
     let mut window: PistonWindow =
-        WindowSettings::new("piston: paint", (width, height))
+        WindowSettings::new("test", (x, y))
         .exit_on_esc(true)
         .graphics_api(opengl)
         .build()
         .unwrap();
-
-    let mut canvas = im::ImageBuffer::new(width, height);
     let mut texture_context = TextureContext {
         factory: window.factory.clone(),
         encoder: window.factory.create_command_buffer().into()
     };
+    let mut events = Events::new(EventSettings::new().lazy(false));
+    let buf = im::ImageBuffer::new(x, y);
     let mut texture: G2dTexture = Texture::from_image(
-            &mut texture_context,
-            &canvas,
-            &TextureSettings::new()
-        ).unwrap();
-
-    let mut c = 0;
-    while let Some(e) = window.next() {
-        texture.update(&mut texture_context, &canvas).unwrap();
-        window.draw_2d(&e, |c, g, device| {
-            // Update texture before rendering.
-            texture_context.encoder.flush(device);
-
-            // clear([1.0; 4], g);
-            image(&texture, c.transform, g);
-        });
-        canvas.put_pixel(c, c, im::Rgba([c as u8, c as u8, c as u8, 255]));
-        c += 1;
-        if c >= width || c >= height{
-            c = 0;
+                &mut texture_context,
+                &buf,
+                &TextureSettings::new()
+            ).unwrap();
+    while let Some(e) = events.next(&mut window) {
+        if let Some(_) = e.render_args() {
+            texture.update(&mut texture_context, &buf).unwrap();
+            window.draw_2d(&e, |c, g, device| {
+                    texture_context.encoder.flush(device);
+                    image(&texture, c.transform, g);
+            });
         }
     }
 }
