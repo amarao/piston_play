ee0656afdeee7e6ebcb45a827e4351132c4d0c3e
George Shuklin <george.shuklin@gmail.com>
2020-09-12 14:18:12 +0300

    Resize operation (WIP)

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,23 @@ struct DrawCommand {
     y: u32,
     color: im::Rgba<u8>
 }
+#[derive(Debug)]
+enum Command {
+    Count(u32),
+    NewResolution(u32, u32),
+}
+
+ #[derive(Debug)]
+struct ControlCommand{
+    command: Command
+}
+// fn scale<T>(buf: T, x:u32, y:u32, new_x:u32, new_y:u32) -> T{
+//     buf
+// }
 
 fn main() {
-    let x = 1920;
-    let y  = 1080;
+    let mut x = 1920;
+    let mut y  = 1080;
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow =
         WindowSettings::new("test", (x, y))
@@ -28,22 +41,33 @@ fn main() {
         factory: window.factory.clone(),
         encoder: window.factory.create_command_buffer().into()
     };
-    let mut buf = im::ImageBuffer::new(x, y);
+    let mut buf = im::ImageBuffer::from_fn(x, y, |_, __| { im::Rgba([255,255,255,255])});
     let mut texture: G2dTexture = Texture::from_image(
                 &mut texture_context,
                 &buf,
                 &TextureSettings::new()
             ).unwrap();
     let mut events = Events::new(EventSettings::new().lazy(false));
-    let (tx,rx): (SyncSender<DrawCommand>, Receiver<DrawCommand>) = mpsc::sync_channel(128);
+    let (draw_tx, draw_rx): (SyncSender<DrawCommand>, Receiver<DrawCommand>) = mpsc::sync_channel(128);
+    let (control_tx, control_rx): (SyncSender<ControlCommand>, Receiver<ControlCommand>) = mpsc::sync_channel(8);
     thread::spawn(move ||{
-            calc(tx,x,y)
+            calc(draw_tx, control_rx, x, y)
     });
     while let Some(e) = events.next(&mut window) {
-        if let Some(_) = e.render_args() {
-            while let Ok(command) = rx.try_recv(){
-                buf.put_pixel(command.x,command.y,command.color);
+        if let Some(draw_event) = e.render_args() {
+            if draw_event.draw_size[0] != x || draw_event.draw_size[1] != y{
+                let new_x = draw_event.draw_size[0];
+                let new_y = draw_event.draw_size[1];
+                println!("Resolution change from {}x{} to {}x{}", x, y, new_x, new_y);
+                control_tx.send(ControlCommand{command: Command::NewResolution(new_x, new_y)}).unwrap();
+                // let mut buf = scale(buf, x, y, new_x, new_y);
             }
+            let mut c = 0;
+            while let Ok(command) = draw_rx.try_recv(){
+                buf.put_pixel(command.x,command.y,command.color);
+                c+=1;
+            }
+            control_tx.send(ControlCommand{command: Command::Count(c)}).unwrap();
             texture.update(&mut texture_context, &buf).unwrap();
             window.draw_2d(&e, |c, g, device| {
                     texture_context.encoder.flush(device);
@@ -54,13 +78,31 @@ fn main() {
     }
 }
 
-fn calc(tx: SyncSender<DrawCommand>, max_x:u32, max_y:u32){
+fn calc(draw: SyncSender<DrawCommand>, command: Receiver<ControlCommand>, max_x:u32, max_y:u32){
     let mut rng = rand::thread_rng();
     loop{
+        loop {
+            match command.try_recv() {
+                Err(std::sync::mpsc::TryRecvError::Empty) => break,
+                Err(std::sync::mpsc::TryRecvError::Disconnected) => return,
+                Ok(cmd) => {
+                    match cmd.command {
+                        Command::Count(counter) => {
+                                println!("counter: {}", counter);
+                            },
+                        Command::NewResolution(new_x, new_y) => {
+                                println!("new resolution:{}x{}", new_x, new_y);
+                            },
+                    }
+                }
+            }
+        }
         let x = rng.gen_range(0,max_x);
         let y = rng.gen_range(0,max_y);
         let color = im::Rgba([rng.gen_range(0,255), rng.gen_range(0,255), rng.gen_range(0,255), rng.gen_range(0,255)]);
-        if let Err(_)  = tx.send(DrawCommand{x, y, color}) {break}
+        if let Err(_)  = draw.send(DrawCommand{x, y, color}){
+            break
+        }
         thread::sleep(Duration::from_millis(1));
     }
 }
