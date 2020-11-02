5f22afca14a0ed0c441b4e8510c9a22807967bf2
George Shuklin <george.shuklin@gmail.com>
2020-11-02 22:17:29 +0200

    Remove redundant x/y in buffer.

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,12 @@ use gfx_device_gl;
 
 #[derive(Debug)]
 pub struct Buffer{
-    x: u32,
-    y: u32,
     buf: im::ImageBuffer<im::Rgba<u8>,Vec<u8>>
 }
 
 impl Buffer{
     pub fn new(x:u32, y:u32)-> Self{
         Buffer{
-            x: x,
-            y: y,
             buf: im::ImageBuffer::from_fn(x, y, |_, __| { im::Rgba([255,255,255,255]) })
         }
     }
@@ -24,16 +20,16 @@ impl Buffer{
     }
 
     pub fn scale(&mut self, new_x:u32, new_y:u32){
+        let old_x = self.buf.width();
+        let old_y = self.buf.height();
         let new_buf:im::ImageBuffer<im::Rgba<u8>,Vec<u8>> = im::ImageBuffer::from_fn(new_x, new_y, |x, y| {
-            if x < self.x && y < self.y {
+            if x < old_x && y < old_y {
                 *(self.buf.get_pixel(x, y))
             }else{
                 im::Rgba([255,255,255,255])
             }
         });
         self.buf = new_buf;
-        self.x = new_x;
-        self.y = new_y;
     }
 
     pub fn buf_ref(&self) -> &im::ImageBuffer<im::Rgba<u8>,Vec<u8>>{
@@ -60,8 +56,6 @@ impl Buffer{
 
     pub fn clone(&self) -> Self{
         Buffer{
-            x: self.x,
-            y: self.y,
             buf: self.buf.clone()
         }
     }
